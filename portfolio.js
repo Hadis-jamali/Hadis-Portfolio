ff910e2c4ccbf6478f4ca92a3fe8adb6d7e862d3
Hadis Jamali <hadisjamali95@gmail.com>
2024-09-11 11:40:32 +0200

    Responsive Navigation Bar

--- portfolio.js
+++ portfolio.js
@@ -1,2 +1,10 @@
 //Responsive Navigation Bar
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-list");
 
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
+
+  // Burger Animation
+  burger.classList.toggle("toggle");
+});
